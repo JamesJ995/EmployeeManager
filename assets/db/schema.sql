d5ca6631e44639f3bad94ad36eb2d5fe9ebf4de3
jamesj995 <mrjam7@gmail.com>
2021-07-29 17:53:07 -0500

    added schema and seeds, working db print

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -1,9 +1,27 @@
-DROP DATABASE IF EXISTS employeee_management_db;
+DROP DATABASE IF EXISTS employee_management_db;
 CREATE DATABASE employee_management_db;
 
 USE employee_management_db;
 
-CREATE TABLE employee (
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
+  ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -12,19 +30,4 @@ CREATE TABLE employee (
   FOREIGN KEY(role_id) REFERENCES roles(id),
   FOREIGN KEY(manager_id) REFERENCES roles(id)
   ON DELETE SET NULL
-);
-
-CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(5,2),
-  department_id INT,
-  FOREIGN KEY (department_id)
-  REFERENCES departments(id)
-  ON DELETE SET NULL
-);
-
-CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
 );
---
+++ assets/db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO departments (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO roles (title, salary)
+VALUES ("Software Engineer", 100000),
+       ("Accountant", 80000),
+       ("Lawyer", 200000),
+       ("Salesperson", 150000),
+       ("Sales Lead", 300000);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ("Alexa", "Mathis", 1),
+       ("Haris", "Franco", 2),
+       ("Kathryn", "Adamson", 3),
+       ("Miles", "Lancaster", 4),
+       ("Darcey", "Frey", 5);
